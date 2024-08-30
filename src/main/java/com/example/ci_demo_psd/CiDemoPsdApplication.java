f9a60ec3f8e90427ad93a3d7ffb64c81b3ff4d61
emrouz.rahat <emrouz.rahat@bjitgroup.com>
2024-08-30 09:41:50 +0600

    Initial commit

---
+++ src/main/java/com/example/ci_demo_psd/CiDemoPsdApplication.java
@@ -0,0 +1,13 @@
+package com.example.ci_demo_psd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiDemoPsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiDemoPsdApplication.class, args);
+	}
+
+}
